0a6594947467f78fae6cbb489bf186b6d934f9b3
lgtm <1gtm@users.noreply.github.com>
2021-08-05 09:19:58 -0700

    Generate code for provider@v2.24.1 gen@b90eb9c (#5)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-newrelic-api/apis/alert/v1alpha1/condition_types.go
+++ vendor/kubeform.dev/provider-newrelic-api/apis/alert/v1alpha1/condition_types.go
@@ -94,7 +94,7 @@ type ConditionSpecResource struct {
 	RunbookURL *string `json:"runbookURL,omitempty" tf:"runbook_url"`
 	// +kubebuilder:validation:MinItems=1
 	Term []ConditionSpecTerm `json:"term" tf:"term"`
-	// The type of condition. One of: (apm_jvm_metric, apm_kt_metric, browser_metric, mobile_metric, servers_metric, apm_app_metric).
+	// The type of condition. One of: (apm_app_metric, apm_jvm_metric, apm_kt_metric, browser_metric, mobile_metric, servers_metric).
 	Type *string `json:"type" tf:"type"`
 	// A custom metric to be evaluated.
 	// +optional
