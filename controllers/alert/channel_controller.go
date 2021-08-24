1b743c0fc939fc73f28e5dbca0ae49261e292220
lgtm <1gtm@users.noreply.github.com>
2021-08-24 01:23:28 -0700

    Generate code for provider@v2.24.1 gen@927fb68 (#6)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- controllers/alert/channel_controller.go
+++ controllers/alert/channel_controller.go
@@ -21,6 +21,7 @@ package alert
 import (
 	"context"
 
+	"github.com/go-errors/errors"
 	"github.com/go-logr/logr"
 	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	auditlib "go.bytebuilders.dev/audit/lib"
@@ -36,7 +37,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // ChannelReconciler reconciles a Channel object
@@ -45,11 +48,10 @@ type ChannelReconciler struct {
 	Log    logr.Logger
 	Scheme *runtime.Scheme
 
-	Gvk              schema.GroupVersionKind // GVK of the Resource
-	Provider         *tfschema.Provider      // returns a *schema.Provider from the provider package
-	Resource         *tfschema.Resource      // returns *schema.Resource
-	TypeName         string                  // resource type
-	WatchOnlyDefault bool
+	Gvk      schema.GroupVersionKind // GVK of the Resource
+	Provider *tfschema.Provider      // returns a *schema.Provider from the provider package
+	Resource *tfschema.Resource      // returns *schema.Resource
+	TypeName string                  // resource type
 }
 
 // +kubebuilder:rbac:groups=alert.newrelic.kubeform.com,resources=channels,verbs=get;list;watch;create;update;patch;delete
@@ -58,10 +60,6 @@ type ChannelReconciler struct {
 func (r *ChannelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := r.Log.WithValues("channel", req.NamespacedName)
 
-	if r.WatchOnlyDefault && req.Namespace != v1.NamespaceDefault {
-		log.Info("Only default namespace is supported for Kubeform Community, Please upgrade to Kubeform Enterprise to use any namespace.")
-		return ctrl.Result{}, nil
-	}
 	var unstructuredObj unstructured.Unstructured
 	unstructuredObj.SetGroupVersionKind(r.Gvk)
 
@@ -81,7 +79,7 @@ func (r *ChannelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	return ctrl.Result{}, err
 }
 
-func (r *ChannelReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager, auditor *auditlib.EventPublisher) error {
+func (r *ChannelReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager, auditor *auditlib.EventPublisher, restrictToNamespace string) error {
 	if auditor != nil {
 		if err := auditor.SetupWithManager(ctx, mgr, &alertv1alpha1.Channel{}); err != nil {
 			klog.Error(err, "unable to set up auditor", alertv1alpha1.Channel{}.APIVersion, alertv1alpha1.Channel{}.Kind)
@@ -99,6 +97,46 @@ func (r *ChannelReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manag
 				return (e.ObjectNew.(metav1.Object)).GetDeletionTimestamp() != nil || !meta_util.MustAlreadyReconciled(e.ObjectNew)
 			},
 		}).
-		Owns(&v1.Secret{}).
+		WithEventFilter(predicate.NewPredicateFuncs(func(e client.Object) bool {
+			if restrictToNamespace != "" && e.GetNamespace() != restrictToNamespace {
+				klog.Infof("Only %s namespace is supported for Kubeform Community. Please upgrade to Kubeform Enterprise to use any namespace.", restrictToNamespace)
+				return false
+			}
+			return true
+		})).
+		Watches(
+			&source.Kind{Type: &v1.Secret{}},
+			handler.EnqueueRequestsFromMapFunc(r.SensitiveSecretWatch(ctx)),
+		).
 		Complete(r)
+}
+
+func (r *ChannelReconciler) SensitiveSecretWatch(ctx context.Context) handler.MapFunc {
+	log := ctrl.LoggerFrom(ctx)
+	return func(o client.Object) []ctrl.Request {
+		result := []ctrl.Request{}
+
+		sensSec, ok := o.(*v1.Secret)
+		if !ok {
+			log.Error(errors.Errorf("expected a Secret but go a %T", o), "failed to get secret for Channel")
+			return nil
+		}
+
+		secName := sensSec.Name
+		secNamespace := sensSec.Namespace
+
+		resourceList := &alertv1alpha1.ChannelList{}
+		if err := r.List(ctx, resourceList, client.InNamespace(secNamespace)); err != nil {
+			log.Error(err, "failed to list Channel")
+			return nil
+		}
+
+		for _, res := range resourceList.Items {
+			if res.Spec.SecretRef.Name == secName {
+				name := client.ObjectKey{Namespace: res.Namespace, Name: res.Name}
+				result = append(result, ctrl.Request{NamespacedName: name})
+			}
+		}
+		return result
+	}
 }
