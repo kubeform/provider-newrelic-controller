09f5a9a5ca4feef427d04aa0fcb538caabca1a5f
1gtm <1gtm@appscode.com>
2021-10-29 11:11:31 +0000

    Generate code for provider@v2.24.1 gen@8e31385
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-newrelic-api/apis/alert/v1alpha1/channel_types.go
+++ vendor/kubeform.dev/provider-newrelic-api/apis/alert/v1alpha1/channel_types.go
@@ -131,7 +131,7 @@ type ChannelSpecResource struct {
 	Config *ChannelSpecConfig `json:"config,omitempty" tf:"config"`
 	// (Required) The name of the channel.
 	Name *string `json:"name" tf:"name"`
-	// (Required) The type of channel. One of: (slack, user, victorops, webhook, email, opsgenie, pagerduty).
+	// (Required) The type of channel. One of: (user, victorops, webhook, email, opsgenie, pagerduty, slack).
 	Type *string `json:"type" tf:"type"`
 }
 
